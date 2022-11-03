3213ede336fb2ae3f62e5776c75e527047774495
GFX23 <Spacil-Tomas@seznam.cz>
2022-11-03 19:06:08 +0100

    Update duplicatingZeros.ts

--- duplicatingZeros.ts
+++ duplicatingZeros.ts
@@ -1,7 +1,7 @@
 // Given array of number, duplicate every zero you will find.
 // Length should remain the same.
 
-// Expl. Iterating through array with for loop, when i encounter "zero", i will splice array in current idx+1, insert one zero and poping last element for maintain length of array
+// Expl. Iterating through array with for loop, when i encounter "zero", i will splice array in current idx+1, insert one zero and poping last element for maintaining length of array
 
 const duplicateZeros = (arr: number[]): void => {
   for (let i = 0; i < arr.length; i++) {
