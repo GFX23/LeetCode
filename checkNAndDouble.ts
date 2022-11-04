29cc3a1f6d530c4c1d9355aa6b9bcfab7099df47
GFX23 <Spacil-Tomas@seznam.cz>
2022-11-04 17:58:14 +0100

    Create checkNAndDouble.ts

---
+++ checkNAndDouble.ts
@@ -0,0 +1,19 @@
+// Desc. Given array of int, check if there exist two int i and j which:
+// i != j
+// 0 <= i, j < arr.length
+// arr[i] == 2 * arr[j]
+
+// Expl.
+
+const checkIfExist = (arr: number[]): boolean => {
+  for (let i = 0; i < arr.length; i++) {
+      if (arr.some((num, idx) => num === arr[i] * 2 && idx !== i )) {
+          return true
+      }
+  }
+  return false
+};
+
+console.log(checkIfExist([10,2,5,3]))
+console.log(checkIfExist([0,0]))
+console.log(checkIfExist([-2,0,10,-19,4,6,-8]))
