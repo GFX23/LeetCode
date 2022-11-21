a4291c91c1e04328e3d20c71c051d7be504287b6
GFX23 <Spacil-Tomas@seznam.cz>
2022-11-21 18:48:17 +0100

    Create 53-maxSubArray.ts

---
+++ 53-maxSubArray.ts
@@ -0,0 +1,3 @@
+// Desc.: Given an integer array nums, find the subarray which has the largest sum and return its sum.
+
+// Expl.: Iterating through sorted array, while idx === idx+1, returning true, else false
