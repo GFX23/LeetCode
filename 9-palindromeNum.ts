8ccbda8bbc31dd193f3e8caf772563de1953dace
GFX23 <Spacil-Tomas@seznam.cz>
2022-11-27 12:17:05 +0100

    Update 9-palindromeNum.ts

--- 9-palindromeNum.ts
+++ 9-palindromeNum.ts
@@ -2,12 +2,12 @@
 
 // Expl.: Convert to string, split, reverse, join => return
 
-const isPalindrome = (x: number): boolean => {
-  const reversed: string = x.toString().split("").reverse().join("")
-  return x === parseInt(reversed)
-}
+const isPalindromeNum = (x: number): boolean => {
+  const reversed: string = x.toString().split("").reverse().join("");
+  return x === parseInt(reversed);
+};
 
-console.log(isPalindrome(787))
-console.log(isPalindrome(111111))
-console.log(isPalindrome(456))
-console.log(isPalindrome(879545))
+console.log(isPalindromeNum(787));
+console.log(isPalindromeNum(111111));
+console.log(isPalindromeNum(456));
+console.log(isPalindromeNum(879545));
