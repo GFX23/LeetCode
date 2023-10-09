6bf7616e86c683803c90f6f4a04329ab53cee1c9
GFX23 <Spacil-Tomas@seznam.cz>
2023-10-09 09:33:07 +0200

    added another solution

---
+++ 441-arrangingCoins.ts
@@ -0,0 +1,49 @@
+// ---------------- LEETCODE TASK DESCRIPTION ----------------- //
+// ------------------ 441 - ARRANGING COINS ------------------- //
+
+/* You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+ 
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3. */
+
+// ------------------------ SOLUTION -------------------------- //
+
+// 
+// Time complexity: O(sqrt(n))
+// Space complexity: O(1)
+
+const arrangeCoins = (n:number): number => {
+  let row = 1
+
+  while (n >= row) {
+    console.log("div")
+    n -= row
+    row++
+  }
+  return row -1
+}
+
+
+
+// --------------------------- LOGS --------------------------- //
+
+console.log(arrangeCoins(5)) //2
+console.log(arrangeCoins(10)) //4
+console.log(arrangeCoins(8)) //3
+console.log(arrangeCoins(0)) //0
+
+// ----------------------- CLI COMMAND ------------------------ //
+
+//run ts-node 441-arrangingCoins.ts
