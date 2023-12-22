8338666876907600e075e9a71fdd060e5cf3d5c8
GFX23 <Spacil-Tomas@seznam.cz>
2023-12-22 10:09:35 +0100

    leetcode daily 22.12.23

---
+++ 2706-buyTwoChocolates.ts
@@ -0,0 +1,43 @@
+// ---------------- LEETCODE TASK DESCRIPTION ----------------- //
+// ---------------- 2706 - BUY TWO CHOCOLATES ----------------- //
+
+/* You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+
+You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
+
+Example 1:
+
+Input: prices = [1,2,2], money = 3
+Output: 0
+Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
+Example 2:
+
+Input: prices = [3,2,3], money = 3
+Output: 3
+Explanation: You cannot buy 2 chocolates without going in debt, so we return 3. */
+
+// ------------------------ SOLUTION -------------------------- //
+
+// Sort the array, then subtract the first two elements from money. If the result is positive, return it. If not, return money.
+// Time complexity: O(n log n)
+// Space complexity: O(1)
+
+const buyChoco = (prices: number[], money: number): number => {
+  prices.sort((a,b) => a-b)
+  let sum = money - (prices[0]+prices[1])
+  return sum >= 0 ? sum : money 
+};
+
+
+
+// --------------------------- LOGS --------------------------- //
+
+console.log(buyChoco([1,2,2], 3))
+console.log(buyChoco([3,2,3], 3))
+console.log(buyChoco([1,2,3,4,5], 10))
+
+// ----------------------- CLI COMMAND ------------------------ //
+
+//run ts-node 2706-buyTwoChocolates.ts
